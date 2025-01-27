0fac572a7b5cd7c674880bf2867ff70ebf1c4cff
Oliviagiles09 <giles11@usf.edu>
2025-01-26 21:25:54 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,4 @@
+// Task 1 
 let employeeName = "NatalieRob";
 const employeeID = 7654;
 var isActive = true;
@@ -5,3 +6,12 @@ var isActive = true;
 consol.log("employeeName:", employeeName,"Type:", typeof employeeName);
 consol.log("employeeID:", employeeID, "Type:", typeof employeeID);
 consol.log("isActive:", isActive, "type:", typeof isActive);
+
+// Task 2 
+let productName = "Hair Dryer";
+const productPrice = 429.99;
+var isAvailable = true;
+
+consol.log("productName:", productName, "Type:", typeof productName);
+consol.log("productPrice:", productPrice, "Type:", typeof productPrice);
+consol.log("isAvaiable:", isAvailable, "Type:",typeof isAvailable);
