fe47f5e39ad1b2b311e94a26f0ff00eaffe9411f
Oliviagiles09 <giles11@usf.edu>
2025-01-26 21:16:56 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+let employeeName = "NatalieRob";
+const employeeID = 7654;
+var isActive = true;
+
+consol.log("employeeName:", employeeName,"Type:", typeof employeeName);
+consol.log("employeeID:", employeeID, "Type:", typeof employeeID);
+consol.log("isActive:", isActive, "type:", typeof isActive);
